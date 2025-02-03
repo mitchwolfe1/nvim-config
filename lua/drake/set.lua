9634d653b867364a5d76a32bb125fb9f4c06e561
Mitch <x0rzinq@gmail.com>
2025-02-02 20:48:41 -0800

    feat: 2 spaces for webdev

--- lua/drake/set.lua
+++ lua/drake/set.lua
@@ -9,6 +9,15 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.expandtab = true
+    end,
+})
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
