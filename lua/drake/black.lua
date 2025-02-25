a738531ffc181f3d9716180d6fb8c9d77a287d1a
Mitch <x0rzinq@gmail.com>
2025-02-24 17:24:56 -0800

    fix: remove black formatting in favor of null ls, removed startup cuz its ugly

--- lua/drake/black.lua
+++ lua/drake/black.lua
@@ -3,25 +3,23 @@ function format_with_black()
     local buf = vim.api.nvim_get_current_buf()
     local filename = vim.api.nvim_buf_get_name(buf)
 
-    vim.fn.jobstart({'isort', filename}, {
+    vim.fn.jobstart({ 'isort', filename }, {
         on_exit = function()
+            vim.api.nvim_command('checktime')
             -- After isort completes, run black
-            vim.fn.jobstart({'black', filename}, {
-                on_exit = function()
-                    vim.api.nvim_command('checktime')  -- Check if the file has changed and reload
-                end,
-            })
+            -- vim.fn.jobstart({'black', filename}, {
+            --     on_exit = function()
+            --         vim.api.nvim_command('checktime')  -- Check if the file has changed and reload
+            --     end,
+            -- })
         end,
     })
 end
 
--- Set up an autocmd to format on save
+-- -- Set up an autocmd to format on save
 vim.cmd([[
 augroup fmt
     autocmd!
     autocmd BufWritePre *.py lua format_with_black()
 augroup END
 ]])
-
-
-
