aed3ae661e466054b5d845df179db0256026bc87
Mitch <x0rzinq@gmail.com>
2026-08-28 14:55:13 -0700

    fix: restore Tree-sitter compatibility on Neovim 0.12

--- lua/drake/lazy/treesitter.lua
+++ lua/drake/lazy/treesitter.lua
@@ -1,19 +1,53 @@
+local ensure_installed = {
+  "c",
+  "html",
+  "javascript",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "ruby",
+  "rust",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = {'EdenEast/nightfox.nvim'},
+  branch = "main",
+  lazy = false,
+  dependencies = { "EdenEast/nightfox.nvim" },
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c", "lua", "vim", "vimdoc", "javascript", "html", "python", "typescript", "ruby", "rust"
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = { enable = true,
-       additional_vim_regex_highlighting = true,
-      },
-      indent = { enable = true },
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup({
+      install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site"),
     })
-  end
+
+    treesitter.install(ensure_installed)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("drake_treesitter", { clear = true }),
+      pattern = "*",
+      callback = function(args)
+        local filetype = vim.bo[args.buf].filetype
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
+
+        local started = pcall(vim.treesitter.start, args.buf, language)
+        if not started then
+          return
+        end
+
+        local has_indents, indent_query = pcall(vim.treesitter.query.get, language, "indents")
+        if has_indents and indent_query then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
 }
